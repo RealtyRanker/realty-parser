a1d6fb1f2cd3f751e3004262753842fc99718e82
Andrey Misnik <asmisnik@avito.ru>
2026-04-20 22:07:28 +0300

    remove all for rewriting into Golang

--- src/backgrounds/latest_flats_listener.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <userver/components/component_list.hpp>
-
-namespace realty-parser {
-void EnableLatestFlatsListener(userver::components::ComponentList& component_list);
-}  // namespace realty-parser
--- src/handlers/flats_history.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <userver/components/component_list.hpp>
-
-namespace realty-parser {
-void AppendFlatsHistoryHandler(userver::components::ComponentList& component_list);
-}  // namespace realty-parser
